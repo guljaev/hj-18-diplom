09bb2185599fe2b727dc8b6a5c902df870a74a7c
Ivan Gulyaev <iv.guljaev@gmail.com>
2018-04-05 21:59:00 +0300

    remove needless stuff from draw code

--- js/solution.js
+++ js/solution.js
@@ -463,8 +463,6 @@ function throttleImg(callback) {
     };
 }
 
-const BRUSH_RADIUS = 4;
-
 function createCanvas() {
     const width = getComputedStyle(wrap.querySelector('.current-image')).width.slice(0, -2);
     const height = getComputedStyle(wrap.querySelector('.current-image')).height.slice(0, -2);
@@ -483,127 +481,99 @@ function createCanvas() {
     wrap.insertBefore(canvas, wrap.querySelector('.current-image').nextElementSibling);
 }
 
-const undoBtn = document.getElementById("undo");
-const redoBtn = document.getElementById("redo");
-const clearBtn = document.getElementById("clear");
+const BRUSH_RADIUS = 4;
 const ctx = canvas.getContext("2d");
-ctx.strokeStyle = 'red';
-ctx.fillStyle = 'red';
-
+// ctx.strokeStyle = '#6cbe47';
+// ctx.fillStyle = '#6cbe47';
 let curves = [];
-let undone = [];
 let drawing = false;
-let weird = false;
 let needsRepaint = false;
 
 // curves and figures
 function circle(point) {
-  ctx.beginPath();
-  ctx.arc(...point, BRUSH_RADIUS / 2, 0, 2 * Math.PI);
-  ctx.fill();
+    ctx.beginPath();
+    ctx.arc(...point, BRUSH_RADIUS / 2, 0, 2 * Math.PI);
+    ctx.fill();
 }
 
 function smoothCurveBetween(p1, p2) {
-  // Bezier control point
-  const cp = p1.map((coord, idx) => (coord + p2[idx]) / 2);
-  ctx.quadraticCurveTo(...p1, ...cp);
+    // Bezier control point
+    const cp = p1.map((coord, idx) => (coord + p2[idx]) / 2);
+    ctx.quadraticCurveTo(...p1, ...cp);
 }
 
 function smoothCurve(points) {
-  ctx.beginPath();
-  ctx.lineWidth = BRUSH_RADIUS;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
+    ctx.beginPath();
+    ctx.lineWidth = BRUSH_RADIUS;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
 
-  ctx.moveTo(...points[0]);
+    ctx.moveTo(...points[0]);
 
-  for (let i = 1; i < points.length - 1; i++) {
-    smoothCurveBetween(points[i], points[i + 1]);
-  }
+    for (let i = 1; i < points.length - 1; i++) {
+        smoothCurveBetween(points[i], points[i + 1]);
+    }
 
-  ctx.stroke();
+    ctx.stroke();
 }
 
 // events
-function makePoint(x, y, reflect = false) {
-  return reflect ? [y, x] : [x, y];
+function makePoint(x, y) {
+    return [x, y];
 }
 
-
 canvas.addEventListener("mousedown", (evt) => {
-  drawing = true;
-  weird = evt.shiftKey; // press shift to make things weird =)
-  undone = []; // reset the undone stack
+    drawing = true;
 
-  const curve = []; // create a new curve
+    const curve = []; // create a new curve
 
-  curve.push(makePoint(evt.offsetX, evt.offsetY, weird)); // add a new point
-  curves.push(curve); // add the curve to the array of curves
-  needsRepaint = true;
+    curve.push(makePoint(evt.offsetX, evt.offsetY)); // add a new point
+    curves.push(curve); // add the curve to the array of curves
+    needsRepaint = true;
 });
 
 canvas.addEventListener("mouseup", (evt) => {
-  drawing = false;
+    drawing = false;
 });
 
 canvas.addEventListener("mouseleave", (evt) => {
-  drawing = false;
+    drawing = false;
 });
 
 canvas.addEventListener("mousemove", (evt) => {
-  if (drawing) {
-    // add a point
-    const point = makePoint(evt.offsetX, evt.offsetY, weird);
-    curves[curves.length - 1].push(point);
-    needsRepaint = true;
-  }
+    if (drawing) {
+        // add a point
+        const point = makePoint(evt.offsetX, evt.offsetY);
+        curves[curves.length - 1].push(point);
+        needsRepaint = true;
+    }
 });
 
-// undoBtn.addEventListener("click", (evt) => {
-//   if (curves.length > 0) {
-//     undone.push(curves.pop());
-//   }
-
-//   needsRepaint = true;
-// });
-
-// redoBtn.addEventListener("click", (evt) => {
-//   if (undone.length > 0) {
-//     curves.push(undone.pop());
-//   }
-
-//   needsRepaint = true;
-// });
-
-// clearBtn.addEventListener("click", (evt) => {
-//   curves = [];
-//   undone = [];
-
-//   needsRepaint = true;
-// });
 
 // rendering
 function repaint() {
-  // clear before repainting
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // clear before repainting
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = '#6cbe47';
+    ctx.fillStyle = '#6cbe47';
 
-  curves
-    .forEach((curve) => {
-      // first...
-      circle(curve[0]);
+    curves
+        .forEach((curve) => {
+        // first...
+        circle(curve[0]);
 
-      // the body is compraised of lines
-      smoothCurve(curve);
-    });
+        // the body is compraised of lines
+        smoothCurve(curve);
+        });
 }
 
 function tick() {
-  if (needsRepaint) {
-    repaint();
-    needsRepaint = false;
-  }
+    if (needsRepaint) {
+        repaint();
+        needsRepaint = false;
+    }
 
-  window.requestAnimationFrame(tick);
+    window.requestAnimationFrame(tick);
 }
 
 tick();
